aa1232ab020e25d0b1d1fb59266d274765565edd
amelioa <alyssaamelio@gmail.com>
2023-05-17 08:49:34 -0400

    js

---
+++ examples/js/primitive_data_types.js
@@ -0,0 +1,52 @@
+console.log("Hello World!!!");
+
+console.log(10000);
+console.log("4");
+
+console.log(10 + 5);
+console.log(20 - 12);
+console.log(3 * 4);
+console.log(10 /2);
+
+// Boolean data type (true/false)
+
+console.log(true);
+console.log(false);
+
+console.log(10 == 10);
+console.log(10 == 5);
+
+console.log(10 != 10);
+console.log(10 != 5);
+
+console.log(10 > 5);
+console.log(5 > 10);
+
+console.log(10 < 5);
+console.log(5 < 10);
+
+console.log(10 >= 5);
+console.log(10 >= 10);
+
+// variable
+// let variableName = variableValue;
+let additionResult = 5 + 150;
+console.log(additionResult);
+
+additionResult = 20 + 2;
+console.log(additionResult);
+
+let multiplicationResult = 2 * 3;
+let mixedResult = additionResult + multiplicationResult;
+console.log(multiplicationResult);
+console.log(mixedResult);
+
+// if... else statements
+let a = 5;
+let b = 10;
+
+if (a == b) {
+    console.log("This is the first code path");
+} else {
+    console.log("This is the second code path");
+}
