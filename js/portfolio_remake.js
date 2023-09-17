b35a75792590487298f81e30243ff97e74956874
amelioa <alyssaamelio@gmail.com>
2023-09-17 18:16:26 -0400

    changes

--- js/portfolio_remake.js
+++ js/portfolio_remake.js
@@ -146,22 +146,26 @@ skillsDataElements.forEach(element => {
   delay -= 300; // Decrease delay for each element
 });
 
-// let hamburgerButton = document.getElementById("hamburger");
-// let closeButton = document.getElementById("close");
-// let mobileNav = document.getElementById("mobile_nav");
+// const menu_btn = document.querySelector('.hamburger');
+// const mobile_menu = document.querySelector('.mobile-nav');
 
-// hamburgerButton.onclick = function(){
-//     mobileNav.classList.add("active");
-// };
-
-// closeButton.onclick = function(){
-//     mobileNav.classList.remove("active");
-// };
+// menu_btn.addEventListener('click', function () {
+//     menu_btn.classList.toggle('is-active');
+//     mobile_menu.classList.toggle('is-active');
+// });
 
 const menu_btn = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('.mobile-nav');
+const mobile_nav_links = mobile_menu.querySelectorAll('a');
 
 menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
+});
+
+mobile_nav_links.forEach(function(link) {
+    link.addEventListener('click', function () {
+        mobile_menu.classList.remove('is-active');
+        menu_btn.classList.remove('is-active');
+    });
 });
